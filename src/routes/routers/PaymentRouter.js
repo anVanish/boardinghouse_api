42987b45343e75528ab049b41bf55dacf46ee35c
Vanish <sosvanish@gmail.com>
2024-07-04 21:16:44 +0700

    token for ipn removed

--- src/routes/routers/PaymentRouter.js
+++ src/routes/routers/PaymentRouter.js
@@ -3,8 +3,8 @@ const express = require('express')
 const router = express.Router()
 const {createVNPPayment, checksumVNPPayment, ipnVNPPayment} = require('../../app/controllers/PaymentController')
 
-router.use(authToken, authUser)
 router.get('/vnpay/ipn', ipnVNPPayment)
+router.use(authToken, authUser)
 router.post('/vnpay/checksum/me', checksumVNPPayment)
 router.post('/vnpay/me', createVNPPayment)
 
